9599a20eb1c9f106fcefc6f0aa23406885b6efc2
Dorani <seif.dorani@gmail.com>
2022-11-28 21:29:33 -0500

    edits

--- src/Major_Review_Activities/FoundationsTwo.java
+++ src/Major_Review_Activities/FoundationsTwo.java
@@ -8,13 +8,15 @@ public class FoundationsTwo {
         System.out.println("First example - Area Calculation:");
         double areaOfCircle = area(5.0);
         System.out.println("The area of the circle is: " + areaOfCircle);
-
         double areaOfRectangle = area(5,5);
         System.out.println("The area of the rectangle is: " + areaOfRectangle);
         System.out.println("---------------------------------------------------");
 
-    }
+        //Second example:
+        //Working with equality formulas
 
+    }
+    //Test on data types, conditionals, and double typed functions
     public static double area(double radius) {
         if(radius < 0) {
             return -1;
@@ -22,7 +24,7 @@ public class FoundationsTwo {
         double area = (radius * radius) * Math.PI;
         return area;
     }
-
+    //Test on data types, conditionals, and double typed Overloaded functions
     public static double area(double length, double width) {
         if(length < 0 || width < 0) {
             return -1;
