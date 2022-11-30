dba562d892885fddf8c869f685c1177cb8eac83f
Dorani <seif.dorani@gmail.com>
2022-11-29 20:28:25 -0500

    multiple new sub routines with progressive difficulty, moving on to foundations 3 now

---
+++ src/Major_Review_Activities/FoundationsThree.java
@@ -0,0 +1,11 @@
+package Major_Review_Activities;
+
+public class FoundationsThree {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    //Test on data types, conversions, and void typed functions
+}
