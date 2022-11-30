6b45cc44edf8d21c94bc0408075d6b5c15163864
Dorani <seif.dorani@gmail.com>
2022-11-30 06:00:06 -0500

    added new examples on number manipulation with mods, getting a bit more complex but fun. for the switch statement, we modified to an enhanced version.

--- src/Major_Review_Activities/FoundationsThree.java
+++ src/Major_Review_Activities/FoundationsThree.java
@@ -3,9 +3,44 @@ package Major_Review_Activities;
 public class FoundationsThree {
 
     public static void main(String[] args) {
+        //First example:
+        System.out.println("First example - printDayOfTheWeek:");
+        printDayOfTheWeek(4);
+        System.out.println("---------------------------------------------------");
 
+        //Second example:
+        System.out.println("Second example - printDayOfTheWeek:");
+        System.out.println("The Sum of digits in the number 125 is " + sumDigits(125));
+        System.out.println("---------------------------------------------------");
     }
 
 
     //Test on data types, conversions, and void typed functions
+    //Using enhanced switch statement
+    public static void printDayOfTheWeek(int day) {
+        switch (day) {
+            case 0 -> System.out.println("Sunday");
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+        }
+    }
+
+    //Test on data types, conversions, and void typed functions
+    public static int sumDigits(int number) {
+        if(number < 10) {
+            return -1;
+        }
+        int sum = 0;
+        while (number > 0) {
+            int digits = number % 10;
+            System.out.println(digits);
+            sum += digits;
+            number /= 10;
+        }
+        return sum;
+    }
 }
