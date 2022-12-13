6116ad07779fefa2f76bf2bb368502b8ccdd9313
Dorani <seif.dorani@gmail.com>
2022-12-12 22:48:26 -0500

    fixed the reverse issue with a new method to help us in our main function

--- src/Major_Review_Activities/FoundationsFour.java
+++ src/Major_Review_Activities/FoundationsFour.java
@@ -206,46 +206,33 @@ public class FoundationsFour {
         if (number == 0) {
             System.out.println("Zero");
         } else {
-            while(number > 0) {
-                lastDigit = number % 10;
+            int reversedNumber = reverse(number);
+            while(reversedNumber > 0) {
+                lastDigit = reversedNumber % 10;
                 switch (lastDigit) {
-                    case 1:
-                        System.out.println("One");
-                        break;
-
-                    case 2:
-                            System.out.println("Two");
-                        break;
-
-                    case 3:
-                        System.out.println("Three");
-                        break;
-
-                    case 4:
-                        System.out.println("Four");
-                        break;
-
-                    case 5:
-                        System.out.println("Five");
-                        break;
-
-                    case 6:
-                        System.out.println("Six");
-                        break;
-
-                    case 7:
-                        System.out.println("Seven");
-                        break;
-                    case 8:
-                        System.out.println("Eight");
-                        break;
-
-                    case 9:
-                        System.out.println("Nine");
-                        break;
+                    case 1 -> System.out.println("One");
+                    case 2 -> System.out.println("Two");
+                    case 3 -> System.out.println("Three");
+                    case 4 -> System.out.println("Four");
+                    case 5 -> System.out.println("Five");
+                    case 6 -> System.out.println("Six");
+                    case 7 -> System.out.println("Seven");
+                    case 8 -> System.out.println("Eight");
+                    case 9 -> System.out.println("Nine");
                 }
-                number = number /10;
+                reversedNumber = reversedNumber /10;
             }
         }
     }
+
+    public static int reverse(int number) {
+        int reversed = 0;
+        while(number != 0) {
+            reversed = reversed * 10;
+            reversed += number % 10;
+            number = number / 10;
+        }
+        return reversed;
+    }
+
 }
