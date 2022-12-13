96e02bb8f62023496dcb22eb3cb38387969f9438
Dorani <seif.dorani@gmail.com>
2022-12-12 22:36:42 -0500

    new algo, works, but def needs to be optmized with 2 new sub routines, a reverse, and a perhaps a converter

--- src/Major_Review_Activities/FoundationsFour.java
+++ src/Major_Review_Activities/FoundationsFour.java
@@ -47,6 +47,11 @@ public class FoundationsFour {
         System.out.println("Eighth example - isPerfectNumber: 6");
         System.out.println(isPerfectNumber(6));
         System.out.println("---------------------------------------------------");
+
+        //Ninth example:
+        System.out.println("Ninth example - numberToWords: 123");
+        numberToWords(123);
+        System.out.println("---------------------------------------------------");
     }
     //Function that checks if the reverse of a number is equal to the initial
     public static boolean isPalindrome(int number) {
@@ -191,5 +196,56 @@ public class FoundationsFour {
            }
         }
         return sum == number;
+    }
+
+    public static void numberToWords(int number) {
+        int lastDigit = 0;
+        if (number < 0) {
+            System.out.println("Invalid Input");
+        }
+        if (number == 0) {
+            System.out.println("Zero");
+        } else {
+            while(number > 0) {
+                lastDigit = number % 10;
+                switch (lastDigit) {
+                    case 1:
+                        System.out.println("One");
+                        break;
+
+                    case 2:
+                            System.out.println("Two");
+                        break;
+
+                    case 3:
+                        System.out.println("Three");
+                        break;
+
+                    case 4:
+                        System.out.println("Four");
+                        break;
+
+                    case 5:
+                        System.out.println("Five");
+                        break;
+
+                    case 6:
+                        System.out.println("Six");
+                        break;
+
+                    case 7:
+                        System.out.println("Seven");
+                        break;
+                    case 8:
+                        System.out.println("Eight");
+                        break;
+
+                    case 9:
+                        System.out.println("Nine");
+                        break;
+                }
+                number = number /10;
+            }
+        }
     }
 }
