86ad1c4b6af35eca28b5c3aa29d203bcf6a22b17
Dorani <seif.dorani@gmail.com>
2022-12-14 16:52:22 -0500

    explanation added

--- src/Major_Review_Activities/FoundationsFive.java
+++ src/Major_Review_Activities/FoundationsFive.java
@@ -9,7 +9,11 @@ public class FoundationsFive {
         System.out.println("---------------------------------------------------");
     }
 
-
+        //1. Validation of the parameter values
+        //2. Determine how many bigCount can fill the goal:  As long as goal is greater than or equal to 5, take the goal
+        //and subtract 5 for each bigCount that you have.  Use a while loop to do this and make sure you decrement the number of bigCount for each iteration.
+        //3. Determine how many smallCount can fill the goal:  The remaining amount is what you have to fill with smallCount,
+        //so evaluate that you have enough smallCount to fulfill the "remaining" goal.
     public static boolean canPack(int bigCount, int smallCount, int goal) {
         if((bigCount < 0) || (smallCount < 0) || (goal < 0)) {
             return false;
