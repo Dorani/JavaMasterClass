8b90f9c5c100e007c9f41a9d56d57f2c5fbc33d0
Dorani <seif.dorani@gmail.com>
2022-12-14 12:20:26 -0500

    can pack algo is done

--- src/Major_Review_Activities/FoundationsFive.java
+++ src/Major_Review_Activities/FoundationsFive.java
@@ -4,16 +4,27 @@ public class FoundationsFive {
 
     public static void main(String [] args) {
         //First example:
-        System.out.println("First example - isPalindrome: 11212");
-        System.out.println(canPack(1, 5, 9));
+        System.out.println("First example - canPack: bigCount:2, smallCount:4, Goal: 9  ");
+        System.out.println(canPack(2, 0, 9));
         System.out.println("---------------------------------------------------");
     }
 
     public static boolean canPack(int bigCount, int smallCount, int goal) {
-        if (bigCount < 0 || smallCount < 0 || goal < 0) {
+        if ((bigCount < 0) || (smallCount < 0) || (goal < 0)) {
             return false;
+        } else if((bigCount == 0) && (smallCount == 0) && (goal > 0)) {
+            return false;
+        } else {
+            int actualBigCount = bigCount * 5;
+            for(int x = 0; x <= smallCount; x++) {
+                for(int y = 0; y <=bigCount; y++) {
+                    if(actualBigCount + smallCount == goal) {
+                        return true;
+                    }
+                }
+            }
         }
-        return true;
+        return false;
     }
 
 }
