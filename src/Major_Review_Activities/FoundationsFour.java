c4cbf8f520ff11ae284c5175ce0d856a738650aa
Dorani <seif.dorani@gmail.com>
2022-12-14 23:39:15 -0500

    optimized the isPerfectNumber algo, the logic is that when a number is divisible by 2, there's a corresponding "pair" of number/2...So for number = 28, if it's divisible by 2, then the "pair" is number/2 or 28/2 = 14.  Since 2 is the smallest divisible number, then 14 is the largest divisible number, i.e. you won't find any divisible number between 14 and 28.   1 + 2 + 4 + 7 + 14 = 28.  Thus, we can just short-circuit the loop to stop at half of number or number/2.  This will improve the code performance by looping only half as much.

--- src/Major_Review_Activities/FoundationsFour.java
+++ src/Major_Review_Activities/FoundationsFour.java
@@ -189,7 +189,7 @@ public class FoundationsFour {
         if(number < 1) {
             return false;
         } else {
-           for(int i = 1; i < number; i++) {
+           for(int i = 1; i <= number/2; i++) {
               if(number % i == 0) {
                   sum += i;
               }
