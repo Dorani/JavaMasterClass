c6ee24463e00e2dad6125f099c0998deeea2bfee
Dorani <seif.dorani@gmail.com>
2022-12-28 17:50:55 -0500

    input calc with scanner

--- src/Major_Review_Activities/FoundationsSix.java
+++ src/Major_Review_Activities/FoundationsSix.java
@@ -4,39 +4,39 @@ import java.util.Scanner;
 
 public class FoundationsSix {
     public static void main(String [] args) {
-
+        //First example:
+        System.out.println("First example - canPack: bigCount:2, smallCount:4, Goal: 9  ");
+        inputThenPrintSumAndAverage();
+        System.out.println("---------------------------------------------------");
     }
 
 
     public static void inputThenPrintSumAndAverage() {
         Scanner scanner = new Scanner(System.in);
-
         int sum = 0;
         double average = 0;
-
         int counter = 0;
 
         while(true) {
             System.out.println("Please enter a number that will be summed to the next, then averaged");
             counter++;
             boolean isInt = scanner.hasNextInt();
-            //if number entered is an int
-
+            //if number entered is an int, ie hasInt()
+            if(isInt) {
                 //take that number and add it to the sum var
-
+                int number = scanner.nextInt();
                 //calculate the average, ie,sum / counter
-
+                sum += number;
+                average = Math.round(sum / counter);
                 //round down the average with the Math.round() method
-
-            // else
-
-                //break the if
-
+            } else {
+                break;
+            }
             // go to the next line with scanner.nextLine()
-
-            //print out the sum and average while(false)
-            //close out the scanner instance
+            scanner.nextLine();
         }
-
+        //print out the sum and average while(false)
+        //close out the scanner instance
+        System.out.println("The SUM: " + sum + " and the Average: " + average);
     }
 }
