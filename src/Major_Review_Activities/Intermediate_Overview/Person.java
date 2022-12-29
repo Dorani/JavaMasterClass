220fb8809f014b4d4a9de7c318f630786b12ed2a
Dorani <seif.dorani@gmail.com>
2022-12-29 18:37:37 -0500

    New OOP example, going to keep going

---
+++ src/Major_Review_Activities/Intermediate_Overview/Person.java
@@ -0,0 +1,62 @@
+package Major_Review_Activities.Intermediate_Overview;
+
+public class Person {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setFirstName("");   // firstName is set to empty string
+        person.setLastName("");    // lastName is set to empty string
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");    // firstName is set to John
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");    // lastName is set to Smith
+        System.out.println("fullName= " + person.getFullName());
+    }
+
+        private String firstName;
+        private String lastName;
+        private int age;
+
+        public String getFirstName() {
+            return this.firstName;
+        }
+
+        public String getLastName() {
+            return this.lastName;
+        }
+
+        public int getAge() {
+            return this.age;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public boolean isTeen() {
+            return ((this.age > 12) && (this.age < 20));
+        }
+
+        public String getFullName() {
+            if(this.firstName.isEmpty()) {
+                return getLastName();
+            } else if(this.lastName.isEmpty()) {
+                return getFirstName();
+            }
+            return this.getFirstName() + this.getLastName();
+        }
+
+
+}
---
+++ src/Major_Review_Activities/Intermediate_Overview/SimpleCalculator.java
@@ -0,0 +1,48 @@
+package Major_Review_Activities.Intermediate_Overview;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
+    }
+
+    public static class Calculator {
+        private double firstNumber;
+        private double secondNumber;
+
+        public void setFirstNumber(double firstNumber) {
+            this.firstNumber = firstNumber;
+        }
+
+        public void setSecondNumber(double secondNumber) {
+            this.secondNumber = secondNumber;
+        }
+
+        public double getFirstNumber() { return this.firstNumber;}
+        public double getSecondNumber() { return this.secondNumber;}
+
+        public double getAdditionResult() {
+            return this.firstNumber + this.secondNumber;
+        }
+
+        public double getSubtractionResult() {
+            return this.firstNumber - this.secondNumber;
+        }
+
+        public double getMultiplicationResult() {
+            return this.firstNumber * this.secondNumber;
+        }
+
+        public double getDivisionResult() {
+            return this.firstNumber / this.secondNumber;
+        }
+    }
+}
