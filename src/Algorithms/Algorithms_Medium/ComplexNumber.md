93d0211b2a0f1ced7a81ebaabea39eee3c73950e
Dorani <seif.dorani@gmail.com>
2022-12-30 17:58:41 -0500

    complex number oop complete

--- src/Algorithms/Algorithms_Medium/ComplexNumber.md
+++ src/Algorithms/Algorithms_Medium/ComplexNumber.md
@@ -1,6 +1,9 @@
 #### Complex Operations:
 
-A complex number is a number that can be expressed in the form a + bi, where a and b are real numbers, and i is a solution of the equation x2 = −1. Because no real number satisfies this equation, i is called an imaginary number. For the complex number a + bi, a is called the real part, and b is called the imaginary part. To add or subtract two complex numbers, just add or subtract the corresponding real and imaginary parts. For instance, the sum of 5 + 3i and 4 + 2i is 9 + 5i. For another, the sum of 3 + i and –1 + 2i is 2 + 3i.
+A complex number is a number that can be expressed in the form a + bi, where a and b are real numbers, and i is a solution of the equation x2 = −1. 
+Because no real number satisfies this equation, i is called an imaginary number. For the complex number a + bi, a is called the real part, and b is called the imaginary part. 
+To add or subtract two complex numbers, just add or subtract the corresponding real and imaginary parts. For instance, the sum of 5 + 3i and 4 + 2i is 9 + 5i. 
+For another, the sum of 3 + i and –1 + 2i is 2 + 3i.
 
 Write a class with the name ComplexNumber. The class needs two fields (instance variables) with name real and imaginary of type double. It represents the Complex Number.
 
---
+++ src/Major_Review_Activities/Intermediate_Overview/ComplexNumber.java
@@ -0,0 +1,54 @@
+package Major_Review_Activities.Intermediate_Overview;
+
+public class ComplexNumber {
+
+    public static void main(String[] args) {
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        one.add(1,1);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        one.subtract(number);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        number.subtract(one);
+        System.out.println("number.real= " + number.getReal());
+        System.out.println("number.imaginary= " + number.getImaginary());
+    }
+
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return this.real;
+    }
+
+    public double getImaginary() {
+        return this.imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber complexNumber) {
+        this.real += complexNumber.real;
+        this.imaginary += complexNumber.imaginary;
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber complexNumber) {
+        this.real -= complexNumber.real;
+        this.imaginary -= complexNumber.imaginary;
+    }
+}
