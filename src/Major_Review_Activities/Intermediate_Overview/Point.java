121f9e0eeffaeb35799d4775f01cd781a2e23a78
Dorani <seif.dorani@gmail.com>
2022-12-29 21:13:16 -0500

    some small edits for readability

--- src/Major_Review_Activities/Intermediate_Overview/Point.java
+++ src/Major_Review_Activities/Intermediate_Overview/Point.java
@@ -3,7 +3,13 @@ package Major_Review_Activities.Intermediate_Overview;
 public class Point {
 
     public static void main(String[] args) {
-
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+        Point point = new Point();
+        System.out.println("distance()= " + point.distance());
     }
 
     private int x;
@@ -33,14 +39,17 @@ public class Point {
     }
 
     public double distance() {
-       return (double) Math.sqrt(Math.pow((this.x), 2) + Math.pow((this.y), 2));
+       return (double) Math.sqrt(Math.pow((this.x), 2)
+               + Math.pow((this.y), 2));
     }
 
     public double distance(int x, int y) {
-        return (double) Math.sqrt(Math.pow((x - this.x), 2) + Math.pow((y - this.y), 2));
+        return (double) Math.sqrt(Math.pow((x - this.x), 2)
+                + Math.pow((y - this.y), 2));
     }
 
     public double distance(Point secondPoint) {
-        return (double) Math.sqrt(Math.pow((secondPoint.x - this.x), 2) + Math.pow((secondPoint.y - this.y), 2));
+        return (double) Math.sqrt(Math.pow((secondPoint.x - this.x), 2)
+                + Math.pow((secondPoint.y - this.y), 2));
     }
 }
