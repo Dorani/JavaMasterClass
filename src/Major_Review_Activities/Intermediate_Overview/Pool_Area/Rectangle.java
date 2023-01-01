0581c385cdeb7479a353eba5fbfc88ff51023069
Dorani <seif.dorani@gmail.com>
2023-01-01 16:37:52 -0500

    rectangle pool class which we will be extending to shortly

---
+++ src/Major_Review_Activities/Intermediate_Overview/Pool_Area/Rectangle.java
@@ -0,0 +1,30 @@
+package Major_Review_Activities.Intermediate_Overview.Pool_Area;
+
+public class Rectangle {
+
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        if (length < 0) {
+            length = 0;
+        }
+        this.length = length;
+        if (width < 0) {
+            width = 0;
+        }
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+
+    public double getArea() {
+        return this.length * this.width;
+    }
+}
