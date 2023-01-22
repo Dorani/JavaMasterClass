5a323d2d1e35a5f556b8027855b9f509584a715f
Dorani <seif.dorani@gmail.com>
2023-01-21 21:22:14 -0500

    Polymorphism - java example method implementation finished, tested in main with multiple instances add methods on top

--- src/Major_Review_Activities/Intermediate_Overview_OOP2/Polymorphism/Main.java
+++ src/Major_Review_Activities/Intermediate_Overview_OOP2/Polymorphism/Main.java
@@ -2,6 +2,20 @@ package Major_Review_Activities.Intermediate_Overview_OOP2.Polymorphism;
 
 public class Main {
     public static void main(String[] args) {
+        Car car = new Car("Base car", 8);
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Benz cls65 = new Benz("Cls - 65", 4, true, "S", 24);
+        System.out.println(cls65.startEngine());
+        System.out.println(cls65.accelerate());
+        System.out.println(cls65.brake());
+
+        Lamborghini lamborghini = new Lamborghini("Ghost", 12,2, 240, true);
+        System.out.println(lamborghini.startEngine());
+        System.out.println(lamborghini.accelerate());
+        System.out.println(lamborghini.brake());
 
     }
 }
